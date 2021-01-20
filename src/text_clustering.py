6cf303acb4f5ee748c27da1b495ba1fef1b81d6d
Louis <oneillml@tcd.ie>
2021-01-20 15:03:18 +0000

    prepare to preprocess 20ng by running first 10 layers of longformer, running out of memory on pssr2 when I try this

--- src/text_clustering.py
+++ src/text_clustering.py
@@ -1,31 +1,44 @@
+import numpy as np
+import math
 import torch
 from longformer.sliding_chunks import pad_to_window_size
 from transformers import RobertaTokenizer
-from transformers import LongformerModel
 from sklearn.datasets import fetch_20newsgroups
 from pdb import set_trace
 
-#from transformers import LongformerForSequenceClassification
-#model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096', gradient_checkpointing=True,output_hidden_states=True)
-model = LongformerModel.from_pretrained('allenai/longformer-base-4096', gradient_checkpointing=True,output_hidden_states=True).cuda()
-tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-model.config.max_position_embeddings = 10000
-tokenizer.model_max_length = 10000
+from transformers import LongformerForSequenceClassification
+with torch.cuda.device(3):
+    model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096', gradient_checkpointing=True,output_hidden_states=True).cuda()
+    model.longformer.encoder.layer = model.longformer.encoder.layer[:11]
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+    model.config.max_position_embeddings = 60000
+    tokenizer.model_max_length = 60000
+    model.eval()
 
+    def get_hidden_feature_vec(batch_input):
+        max_in_batch = max([len(item) for item in batch_input])
+        attention_mask = torch.stack([torch.arange(max_in_batch) < len(item) for item in batch_input]).long().cuda()
+        padded = torch.tensor([item + [1]*(max_in_batch - len(item)) for item in batch_input]).long().cuda()
+        padded, attention_mask = pad_to_window_size(padded, attention_mask, 512, 1)
+        all_outputs = model(padded, attention_mask=attention_mask)
+        hidden = all_outputs[1][11]
+        s = hidden*(attention_mask.unsqueeze(-1))
+        # Take mean of non-zero vectors
+        s = s.sum(axis=1)/(attention_mask.sum(axis=1).unsqueeze(1))
+        return s
 
-ng = fetch_20newsgroups()
-tokenized_input = [tokenizer.encode(s) for s in ng.data[:10]]
-max_in_batch = max([len(item) for item in tokenized_input])
-attention_mask = torch.stack([torch.arange(max_in_batch) < len(item) for item in tokenized_input]).long().cuda()
-padded = torch.tensor([item + [1]*(max_in_batch - len(item)) for item in tokenized_input]).long().cuda()
-padded, attention_mask = pad_to_window_size(padded, attention_mask, 512, 1)
-all_outputs = model(padded, attention_mask=attention_mask)
-output = all_outputs[0]
-
-print(max_in_batch)
-print(output.shape)
-# Zero out indices of padding
-s = output*(attention_mask.unsqueeze(-1))
-# Take mean of non-zero vectors
-s = s.sum(axis=1)/(attention_mask.sum(axis=1).unsqueeze(1))
-print(s.shape)
+    ng = fetch_20newsgroups()
+    tokenized_input = [tokenizer.encode(s) for s in ng.data]
+    batch_size = 8
+    feature_vecs_list = []
+    num_batches = math.ceil(len(tokenized_input)/batch_size)
+    for batch_idx in range(num_batches):
+        try:
+            print(batch_idx, num_batches)
+            batch = tokenized_input[batch_idx*batch_size:(batch_idx+1)*batch_size]
+            feature_vecs = get_hidden_feature_vec(batch)
+            assert feature_vecs.shape == (batch_size,768) # batch_size * hidden_size
+            feature_vecs_list.append(feature_vecs.detach().cpu().numpy())
+        except: set_trace()
+    feature_vecs_array = np.stack(feature_vecs_list)
+    np.save('preprocessed_20ng.npy',feature_vecs_array)
